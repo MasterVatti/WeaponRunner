fc7afb06060f574098c07304cc2c2a736a5f583e
“MasterVatti” <v.zamojtin@gmail.com>
2023-11-10 21:47:00 +0300

    added player movement

--- Assets/CodeBase/Player/PlayerMovement.cs
+++ Assets/CodeBase/Player/PlayerMovement.cs
@@ -1,45 +1,45 @@
-// using System.Collections.Generic;
-// using CodeBase.Services;
-// using UnityEngine;
-// using UnityEngine.AI;
-// using Zenject;
-//
-// namespace CodeBase.Player
-// {
-//   public class PlayerMovement : MonoBehaviour
-//   {
-//     [SerializeField] private NavMeshAgent _navMeshAgent;
-//
-//     private IStaticDataService _dataService;
-//     private List<Vector3> _playerStopPositions;
-//
-//     public PlayerMovement()
-//     {
-//       _playerStopPositions = _dataService.ForLevel(1).PlayerStopPositions;
-//     }
-//
-//     [Inject]
-//     private void Constructor(IStaticDataService dataService)
-//     {
-//       _dataService = dataService;
-//     }
-//
-//     private void Start()
-//     {
-//       Vector3 targetPosition = _playerStopPositions[0];
-//       _navMeshAgent.SetDestination(targetPosition);
-//
-//
-//     }
-//
-//     private void FixedUpdate()
-//     {
-//       if (!_navMeshAgent.pathPending && _navMeshAgent.remainingDistance < 0.5f)
-//       {
-//
-//         Vector3 targetPosition = _playerStopPositions.First();
-//         _navMeshAgent.SetDestination(targetPosition);
-//       }
-//     }
-//   }
-// }
+using System.Collections.Generic;
+using CodeBase.Services;
+using UnityEngine;
+using UnityEngine.AI;
+using Zenject;
+
+namespace CodeBase.Player
+{
+  public class PlayerMovement : MonoBehaviour
+  {
+    [SerializeField] private NavMeshAgent _navMeshAgent;
+
+    private IStaticDataService _dataService;
+    private List<Vector3> _playerStopPositions;
+    private int _pointAchievedQuantity;
+
+    [Inject]
+    private void Constructor(IStaticDataService dataService)
+    {
+      _dataService = dataService;
+    }
+
+    private void Start()
+    {
+      _playerStopPositions = _dataService.ForLevel(1).PlayerStopPositions;
+      MoveToPoint();
+    }
+
+    private void FixedUpdate()
+    {
+      if (_pointAchievedQuantity + 1 < _playerStopPositions.Count && !_navMeshAgent.pathPending && _navMeshAgent.remainingDistance < 0.5f)
+      {
+        _pointAchievedQuantity += 1;
+        MoveToPoint();
+      }
+    }
+
+    public void MoveToPoint()
+    {
+      
+      Vector3 targetPosition = _playerStopPositions[_pointAchievedQuantity];
+      _navMeshAgent.SetDestination(targetPosition);
+    }
+  }
+}
